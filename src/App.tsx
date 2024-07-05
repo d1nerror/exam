726d8b9ae85d8e9823ba1475bb607fcdd6650683
Dinos Kapsalas <dinoskapsalas@gmail.com>
2024-07-05 19:41:54 +0300

    install vite, add Select component and basic CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import React from 'react';
+import Select from './components/Select';
+import './App.css';
+
+const App = ()=> {
+    return (
+        <>
+        <Select /> 
+        </>
+        )
 }
 
 export default App;
